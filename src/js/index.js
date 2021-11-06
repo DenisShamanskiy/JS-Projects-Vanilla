78d5ab7b0576600fe76d7eccc19d50545cd786bf
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2021-11-06 14:15:24 +0300

    В Webpack добавлены настройки для работы с несколькими HTML

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import "../styles/index.css";
+
 const numbers = [2, 3, 5];
 
 const doubledNumbers = numbers.map((number) => number * 2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let htmlPageNames = ["hexRandom"];
+let multipleHtmlPlugins = htmlPageNames.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `./src/${name}.html`,
+    filename: `${name}.html`,
+    chunks: [`${name}`],
+  });
+});
 
 module.exports = {
-  entry: { main: "./src/index.js" },
+  entry: { main: "./src/js/index.js", hexRandom: "./src/js/hexrandom.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
     publicPath: "",
   },
   mode: "development",
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
     compress: true,
-    port: 8080,
-    open: true,
+    port: 9000,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader",
-        exclude: /node_modules/,
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      chunks: ["main"],
     }),
     new CleanWebpackPlugin(),
-  ],
+    new MiniCssExtractPlugin(),
+  ].concat(multipleHtmlPlugins),
 };
