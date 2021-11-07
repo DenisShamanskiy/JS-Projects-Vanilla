644581856425817c5cd3b66fd9f143f1fc22a4f5
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2021-11-08 01:23:29 +0300

    Updates

---
+++ navbar.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e=document.querySelector(".navigation-toggle"),t=document.querySelector(".links");e.addEventListener("click",(function(){t.classList.toggle("show-links")}))})();
