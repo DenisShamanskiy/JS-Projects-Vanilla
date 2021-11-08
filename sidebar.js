84b1b59269caa3ae91e248f0b666688205e586b8
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2021-11-08 23:36:19 +0300

    Updates

---
+++ sidebar.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e=document.querySelector(".navigation-toggle"),t=document.querySelector(".close-btn"),c=document.querySelector(".sidebar");e.addEventListener("click",(function(){c.classList.toggle("show-sidebar")})),t.addEventListener("click",(function(){c.classList.remove("show-sidebar")}))})();
